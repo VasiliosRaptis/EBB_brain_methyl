a63afb9ada4af841a7f1a2bf1d1f9c9675964885
Vasilis Raptis <s2118808@ed.ac.uk>
2026-01-30 11:48:08 +0000

    add hsq plots

--- MWAS/x_MWAS_04.3_make_scores.sh
+++ MWAS/x_MWAS_04.3_make_scores.sh
@@ -100,11 +100,9 @@ done
 
 echo "Analysis finished at: $(date)"
 
-### merge all correlation tables
-chunks_finished=$(ls -1 $OUT/scores/EBB/summary/scores.cor.table.chunk* | wc -l)
-if (( $chunks_finished == 49 )); then
-  >$OUT/scores/EBB/summary/scores.cor.table.txt
-  echo -e "cpg\tmodel\tcorr\tcorr.pv\tcorr.lci\tcorr.uci\tr2" > $OUT/scores/EBB/summary/scores.cor.table.txt
-  cat $OUT/scores/EBB/summary/scores.cor.table.txt $OUT/scores/EBB/summary/scores.cor.table.chunk* >> $OUT/scores/EBB/summary/scores.cor.table.txt
-fi
-
+### merge all correlation tables - RUN ONCE
+# >$OUT/scores/EBB/summary/scores.cor.table.header
+# >$OUT/scores/EBB/summary/scores.cor.table.txt
+# echo -e "cpg\tmodel\tcorr\tcorr.pv\tcorr.lci\tcorr.uci\tr2" > $OUT/scores/EBB/summary/scores.cor.table.header
+# cat $OUT/scores/EBB/summary/scores.cor.table.header $OUT/scores/EBB/summary/scores.cor.table.chunk* >> $OUT/scores/EBB/summary/scores.cor.table.txt
+# rm $OUT/scores/EBB/summary/scores.cor.table.header
